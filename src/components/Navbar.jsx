ad4eb4356aaab4fd958a730ac8050c3cfb677322
vaibhav-06 <vaibhavgaunkar06@gmail.com>
2026-07-11 03:04:00 +0530

    centered the fixed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 export default function Navbar(){
     return(
-
-        <nav className="bg-amber-500 h-15 w-7xl mx-auto fixed z-50">
+//most import css property
+        <nav className="bg-amber-500 h-15 w-7xl mx-auto fixed left-1/2 -translate-x-1/2"> 
             <ul className="bg-amber-600 h-full flex flex-row items-center justify-center space-x-20">
                 <li><a href="#home">Home</a></li>
                 <li><a href="#about">About</a></li>
